dfb88ceb05526141d3f914e2588bb2baef6b183f
jayceee1207 <ablayjc7@gmail.com>
2023-05-08 11:14:35 +0800

    Pseudocode

--- Simple_Calculator_App.py
+++ Simple_Calculator_App.py
@@ -2,3 +2,46 @@
 #BSCPE 1-5
 #Assignment 4: Program 1
 #May 6, 2023
+
+#PSEUDOCODE
+#Make a menu where user will choose their option of operation
+#Ask the user to choose one of the four math operations
+
+#While True to rerun the program if the user wish to
+#If option is out of choices, the program will ask their option again
+#if choice is Addition
+    #Get two numbers from the user
+    #Perform the operation
+    #Display the result
+#Add Error Handling
+    #If the user input a string instead of number
+
+#if choice is Subtraction
+    #Get two numbers from the user
+    #Perform the operation
+    #Display the result
+#Add Error Handling
+    #If the user input a string instead of number
+
+#if choice is Multiplication
+    #Get two numbers from the user
+    #Perform the operation
+    #Display the result
+#Add Error Handling
+    #If the user input a string instead of number
+
+#if choice is Division
+    #Get two numbers from the user
+    #Perform the operation
+    #Display the result
+#Add Error Handling
+    #If the user input a string instead of number
+    #If the user input zero (0) in the second number
+
+#Ask the user if they want to try again 
+#If they want to try again
+    #Repeat the program
+    #Continue the program
+#If they do not want to try again
+    #Display Thank you message
+    #Stop the program
